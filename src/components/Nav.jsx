41da0475b223a45b2effdb40a9307618f84119aa
kartikg1809 <kartikg1809@gmail.com>
2024-01-30 18:27:48 +0530

    final commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,19 @@
 const Nav=()=>{
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="images/Nike-Logo.png" alt="logo" className="logo"></img>
-        </div>
-        <div>
-          <ul>
-            <li href="#" >Menu</li>
-            <li href="#">Location</li>
-            <li href="#">About</li>
-            <li href="#">Contact</li>
-          </ul>
-        </div>
-        <div>
-          <button>Login</button>
-        </div>
-      </nav>
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+
+      <button>login</button>
+    </nav>
     </div>
   );
 };
